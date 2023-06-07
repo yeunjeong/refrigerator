7fd530b3af45d80181a74cf015d05631203654a1
yeunjeong <araruu2536@gmail.com>
2023-06-07 18:58:59 +0900

    recipe

--- screens/RecipeScreen.js
+++ screens/RecipeScreen.js
@@ -25,6 +25,18 @@ const recipePageImg = require('../assets/recipe_page2.png');
 const refrigeratorPageImg = require('../assets/refrigerator_page.png');
 const communityPageImg = require('../assets/community_page.png');
 
+const recipeData = [
+    [
+        '돼지고기 김치찌개 달인이 되는 황금레시피',
+        '돼지고기 감자 양파 대파',
+    ], [
+        '즉석 목살 양념구이~ 달큰한 양념소스가 정말 맛있습니다.',
+        '목살 달걀 양파',
+    ], [
+        '양배츠 참치덮밥 / Cabbage tuna bowl of rice',
+        '양배추 참치',
+    ]];
+
 function Recipe({ route, navigation }) {
     const nickname = '하냥이'
 
@@ -62,6 +74,9 @@ function Recipe({ route, navigation }) {
                     fontSize: 16,
                     textAlign: "center",
                     marginTop: 5,
+                    backgroundColor:'rgba(0, 0, 0, 0.3)',
+                    padding:10,
+                    borderRadius:10,
                     }}
                 >
                     {nickname}님의 냉장고 속 재고를 바탕으로 {"\n"}레시피를
@@ -70,14 +85,42 @@ function Recipe({ route, navigation }) {
             </View>
 
 
-            <View style={{ width: "100%", paddingTop: 30, flex: 20 }}>
-                <FlatList
-                    style={{ height: "80%" }}
-                    data={DATA}
-                    renderItem={({ item }) => <Item title={item.title} />}
-                    keyExtractor={(item) => item.id}
-                />
-                <View style={{ paddingTop: "20%" }}>
+            <View style={{ width: "100%", paddingTop: 30, flex: 20,}}>
+                <ScrollView showsVerticalScrollIndicator={false}>
+                    {recipeData.map((recipe, index) => (
+                        <TouchableOpacity key={index} style={{alignItems:'center'}}>
+                            <View style={{
+                                    flexDirection:'row',
+                                    backgroundColor: "white",
+                                    paddingVertical: 15,
+                                    paddingHorizontal: 5,
+                                    borderRadius: 10,
+                                    elevation: 7,
+                                    marginBottom: 30,
+                                    width: "85%",
+                                }}>
+                                <View>
+                                    <Image source={communityPageImg}></Image>
+                                </View>
+                                <View style={{flex:3}}>
+                                    <Text style={{
+                                        fontSize:16,
+                                        color:'#4B4B4B',
+                                    }}>{recipe[0]}</Text>
+                                    <Text style={{
+                                        fontSize:16,
+                                        color:'#0EA371',
+                                    }}>{recipe[1]}</Text>
+                                    
+                                </View>
+                                
+                            </View>
+                        </TouchableOpacity>
+                    
+                    ))}
+                    </ScrollView>
+                
+                <View style={{ paddingTop: 20 }}>
                     <View style={M_style.tab_st}>
                         <TouchableOpacity>
                             <Image source={recipePageImg} style={M_style.tab_ele_st}></Image>
@@ -97,62 +140,6 @@ function Recipe({ route, navigation }) {
 }
 
 export default Recipe;
-
-const DATA = [
-  {
-    id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
-    title: "First Item",
-  },
-  {
-    id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
-    title: "Second Item",
-  },
-  {
-    id: "58694a0f-3da1-471f-bd96-145571e29d72",
-    title: "Third Item",
-  },
-  {
-    id: "bd7acbea-c1b1-46c2-aved5-3ad53abb28ba",
-    title: "First Item",
-  },
-  {
-    id: "3ac68afc-c6a05-48d3-a4f8-fbd91aa97f63",
-    title: "Second Item",
-  },
-  {
-    id: "58694a0f-3da1-471f-zbd96-145571e29d72",
-    title: "Third Item",
-  },
-  {
-    id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28zba",
-    title: "First Item",
-  },
-  {
-    id: "3ac68afc-c605-48d3-a4f8-fbd91aa97zf63",
-    title: "Second Item",
-  },
-];
-
-const Item = ({ title }) => (
-  <View style={{ justifyContent: "center", alignItems: "center" }}>
-    <View
-      style={{
-        backgroundColor: "white",
-        paddingVertical: 15,
-        paddingHorizontal: 5,
-        borderRadius: 10,
-        elevation: 7,
-        marginBottom: 30,
-        width: "85%",
-      }}
-    >
-      <Image
-        source={communityPageImg}
-        style={{ width: 80, height: 60 }}
-      ></Image>
-    </View>
-  </View>
-);
 
 const M_style = StyleSheet.create({
   tab_st: {
@@ -178,8 +165,10 @@ const headerStyles = StyleSheet.create({
     },
     header:{
         flex:2,
-        backgroundColor: colors.headerGreen,
-        justifyContent: 'center',
+        flexDirection: 'row', 
+        //backgroundColor: colors.headerGreen,
+        //backgroundColor: 'yellow',
+        justifyContent: 'space-between',
         paddingHorizontal: 20,
         paddingTop:10,
     },
